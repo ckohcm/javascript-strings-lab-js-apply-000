af80c9a0ffc78c485a776ce5de67ad0aca38f2d9
Catherine <catkcm.16@gmail.com>
2019-04-07 18:51:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var greeting = "test";
+var greeting = "Hello, everybody!";
 
 var specialGuest = "Neil deGrasse Tyson"
 
-var greetSpecialGuest = "" + specialGuest + "!";
+var greetSpecialGuest = "Hello" + specialGuest + "!";
 
 var topic = "space";
 
